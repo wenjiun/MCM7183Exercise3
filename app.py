748965a444637913dfdf4baaddbf20393cfdbe43
Yap Wen Jiun <wenjiun@yahoo.com>
2024-09-06 19:29:38 +0800

    Update app.py
    Update the title

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from dash import Dash, html
 app = Dash(__name__)
 server = app.server
 
-app.layout = html.H2('Hello MMU!')
+app.layout = html.H1('MCM7183 Exercise 3')
 
 if __name__ == '__main__':
     app.run(debug=True)
