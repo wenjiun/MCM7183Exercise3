22e53d5f717f114abf88e755b619e1fd9331baae
Yap Wen Jiun <wenjiun@yahoo.com>
2024-09-06 19:10:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from dash import Dash, html
 
+# Testing
+
 app = Dash(__name__)
 server = app.server
 
-app.layout = html.H1('Hello World!')
+app.layout = html.H2('Hello MMU!')
 
 if __name__ == '__main__':
     app.run(debug=True)
