1aba09ac4934a9a7a12a83f64f38a9fc78aef081
Yap Wen Jiun <wenjiun@yahoo.com>
2024-09-06 20:38:08 +0800

    Add the Pie graph

--- app.py
+++ app.py
@@ -10,9 +10,20 @@ df = pd.read_csv("https://raw.githubusercontent.com/wenjiun/MCM7183Exercise3/mai
 subset_Malaysia = df[df['country'].isin(["Malaysia"])]
 fig = px.scatter(subset_Malaysia, x="year", y="gdp")
 
+subset_2020 = df[df['year'].isin([2020])]
+subset_2020_Asia = subset_2020[subset_2020['state'].isin(["Asia"])]
+subset_2020_Africa = subset_2020[subset_2020['state'].isin(["Africa"])]
+subset_2020_America = subset_2020[subset_2020['state'].isin(["America"])]
+subset_2020_Europe = subset_2020[subset_2020['state'].isin(["Europe"])]
+subset_2020_Oceania = subset_2020[subset_2020['state'].isin(["Oceania"])]
+pie_data = [sum(subset_2020_Asia['gdp']),sum(subset_2020_Africa['gdp']),sum(subset_2020_America['gdp']),sum(subset_2020_Europe['gdp']),sum(subset_2020_Oceania['gdp'])];
+mylabels = ["Asia", "Africa", "America", "Europe","Oceania"]
+pie_df = {'Continent': mylabels,'GDP': pie_data}
+fig2 = px.pie(pie_df,values="GDP",names="Continent")
+
 image_path = 'assets/logo-mmu.png'
 
-app.layout = [html.H1('MCM7183 Exercise 3'), html.Img(src=image_path), dcc.Graph(figure=fig)]
+app.layout = [html.H1('MCM7183 Exercise 3'), html.Img(src=image_path), dcc.Graph(figure=fig), dcc.Graph(figure=fig2)]
 
 if __name__ == '__main__':
     app.run(debug=True)
