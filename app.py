6379b8dbffa3ebdc6a510be514bbd716e8d8a43b
Yap Wen Jiun <wenjiun@yahoo.com>
2024-09-09 20:39:36 +0800

    Fix slider label

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ app.layout = [html.H1('MCM7183 Exercise 3'),
               dcc.Graph(id="graph-scatter"), 
               #dcc.Dropdown([{'label':'2020', 'value':2020}, {'label':'2010', 'value':2010}, 
               #              {'label':'2000', 'value':2000}], 2020, id='dropdown-year'),
-              dcc.Slider(1960, 2020, 5, value=2020, id='slider-year'),
+              dcc.Slider(1960, 2020, 5, value=2020, id='slider-year',
+                         marks = {i: str(i) for i in range(1960, 2021, 5)}),
               dcc.Graph(id="graph-pie")]
 
 @callback(
